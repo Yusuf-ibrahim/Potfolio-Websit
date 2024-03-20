0ddb28f545085efe6275bb547eea27b40575d366
Yusuf-ibrahim <118865910+Yusuf-ibrahim@users.noreply.github.com>
2024-03-20 20:42:46 +0000

    Add files via upload

---
+++ WEB DEV/Script.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.text', {
+    strings: ["Web Developer!", "not Youtuber!", "Ethical Hacker!", "Animator!", "Front-end Developer!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
